69f7bd04946eac65086789af2516e0cbbeab380c
HS <2992746362@qq.com>
2022-05-24 10:44:28 +0800

    second commit

--- src/main/java/com/tj/test.java
+++ src/main/java/com/tj/test.java
@@ -3,5 +3,6 @@ package com.tj;
 public class test {
     public static void main(String[] args) {
         System.out.println("good moroning");
+        System.out.println("good afternoon");
     }
 }
