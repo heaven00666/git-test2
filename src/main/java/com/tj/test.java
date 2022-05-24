2e964d226fa2215624cd4657935a91b4b14aaca9
HS <2992746362@qq.com>
2022-05-24 10:45:34 +0800

    third commit

--- src/main/java/com/tj/test.java
+++ src/main/java/com/tj/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("good moroning");
         System.out.println("good afternoon");
+        System.out.println("good night");
     }
 }
