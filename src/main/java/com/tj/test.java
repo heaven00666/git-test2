49a2e4a055e5b737ca438a58c857b05058642d8b
HS <2992746362@qq.com>
2022-05-24 11:03:47 +0800

    master commit1

--- src/main/java/com/tj/test.java
+++ src/main/java/com/tj/test.java
@@ -6,5 +6,6 @@ public class test {
         System.out.println("good afternoon");
         System.out.println("good night");
         System.out.println("good morning! hot-fix");
+        System.out.println("master!反了反了");
     }
 }
